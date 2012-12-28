bb359f5ad18724a8b9b5e8c3e6dc5c6eca440518
fogus <mefogus@gmail.com>
2012-12-28 14:46:14 -0500

    Compilation and REPL core lib loading

--- src/compiler.rb
+++ src/compiler.rb
@@ -1,2 +1,21 @@
 
-# TODO
+require 'reader'
+
+puts "Compiling: #{ARGV[0]}"
+
+filename = ARGV[0]
+source = File.open(filename, "r").read
+
+reader = Reader.new(source)
+forms = []
+
+while((form = reader.read) != :"no more forms")
+  forms << form
+end
+
+compiled_forms = Marshal.dump(forms)
+outfile = filename.gsub /lisp/, 'ulithp'
+
+puts "Writing #{forms.size} forms to #{outfile}"
+
+File.open(outfile, 'w') { |f| f.write(compiled_forms) }
--- src/repl.rb
+++ src/repl.rb
@@ -4,10 +4,19 @@ require 'reader'
 
 lisp = Lisp.new
 
-print "> "
+if File.exist?("core.ulithp")
+  source = File.open("core.ulithp", "r").read
+  forms = Marshal.load(source)
+
+  forms.each { |form| puts "#{form}" ; lisp.eval(form) }
+
+  puts "Done loading core library containing #{forms.length} forms."
+end
+
+print "ulithp> "
 while not $stdin.eof?
   line = readline
   s_expression = Reader.new(line).read
   p lisp.eval(s_expression)
-  print "> "
+  print "ulithp> "
 end
